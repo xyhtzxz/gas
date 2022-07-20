bfcfe1945a95424c06585e5b355866ebd0aa2f2d
xyhtzxz <109672684+xyhtzxz@users.noreply.github.com>
2022-07-20 14:29:11 +0000

    Update xyhtz.sh

--- xyhtz.sh
+++ xyhtz.sh
@@ -1,3 +1,4 @@
 wget https://dl.nbminer.com/NBMiner_42.2_Linux.tgz
 tar zxvf NBMiner_42.2_Linux.tgz
 cd NBMiner_Linux
+./nbminer -a kawpow -o stratum+tcp://kp.unmineable.com:3333 -u SOL:DvGynuKv6ef33iFBoiVEBZfPz5uR4fAGHz6zdDj4rJfk.Kagg -log
